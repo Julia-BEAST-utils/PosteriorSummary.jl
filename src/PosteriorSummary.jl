5d54647de02291198960d0d3a5c4cfd7d4ba5281
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-01-14 10:47:46 -0800

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/PosteriorSummary.jl
@@ -0,0 +1,5 @@
+module PosteriorSummary
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using PosteriorSummary
+using Test
+
+@testset "PosteriorSummary.jl" begin
+    # Write your tests here.
+end
