c803f1e180522422c248f9ebc6500d5f77dac7ec
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-01-19 14:09:12 -0800

    using MCMCDiagnostics & adding hpd intervals

--- src/PosteriorSummary.jl
+++ src/PosteriorSummary.jl
@@ -1,5 +1,29 @@
 module PosteriorSummary
 
-# Write your package code here.
+export effective_sample_size,
+       hpd_interval
+
+import MCMCDiagnostics.effective_sample_size
+
+function hpd_interval(x::AbstractVector{Float64}; alpha::Float64 = 0.05, sorted::Bool = false)
+    n = length(x)
+    span = Int(round((1.0 - alpha) * n))
+    smallest_rng = Inf
+    rng_upper = Inf
+    rng_lower = -Inf
+    sx::AbstractVector{Float64} = sorted ? x : sort(x)
+    lower = 0
+    for upper = span:n
+        lower = upper - span + 1
+        rng = sx[upper] - sx[lower]
+        if rng < smallest_rng
+            smallest_rng = rng
+            rng_lower = sx[lower]
+            rng_upper = sx[upper]
+        end
+    end
+
+    return (lower = rng_lower, upper = rng_upper)
+end
 
 end
