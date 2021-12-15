8ba4ce6c9b8e4ccf406c3511c81f59b298e76f79
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-12-15 10:04:56 -0800

    more hpd_interval stuff

--- src/PosteriorSummary.jl
+++ src/PosteriorSummary.jl
@@ -1,7 +1,8 @@
 module PosteriorSummary
 
 export effective_sample_size,
-       hpd_interval
+       hpd_interval,
+       hpd_excludes_value
 
 import MCMCDiagnostics.effective_sample_size
 
@@ -24,6 +25,17 @@ function hpd_interval(x::AbstractVector{Float64}; alpha::Float64 = 0.05, sorted:
     end
 
     return (lower = rng_lower, upper = rng_upper)
+end
+
+function hpd_interval(X::AbstractMatrix{Float64}; args...)
+    p = size(X, 2)
+    hpds = [hpd_interval(@view X[:, i]; args...) for i = 1:p]
+    return hpds
+end
+
+function hpd_excludes_value(x::AbstractVector{Float64}, value::Float64; alpha::Float64 = 0.05, sorted::Bool = false)
+    hpd = hpd_interval(x, alpha = alpha, sorted = sorted)
+    return hpd[1] > value || hpd[2] < value
 end
 
 end
